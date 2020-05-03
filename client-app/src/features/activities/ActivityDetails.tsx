a4e3616766f10e249744e081d55b2216f3b70539
Christopher Floyd <canman33@hotmail.com>
2020-05-03 01:21:01 -0400

    react router, links & nav links, activity from API

--- client-app/src/features/activities/ActivityDetails.tsx
+++ client-app/src/features/activities/ActivityDetails.tsx
@@ -1,12 +1,29 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { Card, Button } from 'semantic-ui-react';
 import ActivityStore from '../../app/stores/activityStore';
 import { observer } from 'mobx-react-lite';
+import { RouteComponentProps, Link } from 'react-router-dom';
+import { LoadingComponent } from '../../app/layout/LoadingComponent';
+
+interface DetailParams {
+  id: string;
+}
 
 
-const ActivityDetails: React.FC = () => {//destructured props
+const ActivityDetails: React.FC<RouteComponentProps<DetailParams>> = ({
+  match,
+  history
+}) => {//========destructured props========
+
   const activityStore = useContext(ActivityStore);
-  const {selectedActivity: activity, openEditForm, cancelSelectedActivity} = activityStore;
+  const {activity, loadActivity, loadingInitial} = activityStore;
+
+  useEffect(() => {
+    loadActivity(match.params.id)
+  }, [loadActivity, match.params.id]);
+
+  if (loadingInitial || !activity) return <LoadingComponent content='loading activity...' />
+
     return (
         <Card fluid>
         <img src={`/assets/images/categoryImages/${activity!.category}.jpg`} alt='array of pictures'/>
@@ -22,13 +39,13 @@ const ActivityDetails: React.FC = () => {//destructured props
         <Card.Content extra>
           <Button.Group widths={2}>
               <Button 
-              onClick={() => openEditForm(activity!.id)}  
+              as={Link} to={`/manage/${activity.id}`} 
               basic 
               color='blue' 
               content='Edit'
               />
               <Button 
-              onClick={cancelSelectedActivity} 
+              onClick={() => history.push('/activities')}
               basic 
               color='grey' 
               content='Cancel'
